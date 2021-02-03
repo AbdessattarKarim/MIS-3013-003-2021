816fd22855e724a9aec9723a74e509928ae81139
karim <karim@LAPTOP-D44LE43O>
2021-02-02 18:33:17 -0600

    Example in class. completed

--- In_Class_Examples/VariablesConversionFormatting/Program.cs
+++ In_Class_Examples/VariablesConversionFormatting/Program.cs
@@ -6,7 +6,23 @@ namespace VariablesConversionFormatting
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            string firstName, lastName;
+            double gpa;
+
+            Console.WriteLine("Please enter your first name >>");
+            firstName = Console.ReadLine();
+
+            Console.WriteLine("Please enetr your last name >>");
+            lastName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your gpa>>");
+            string gpastring = Console.ReadLine();
+            gpa = Convert.ToDouble (gpastring);
+
+            gpa = gpa + .5;
+            string output = "Hello" + firstName + " " + lastName + "!" + "we boostd your gpa to " + gpa.ToString("N3");
+            Console.WriteLine(output);
         }
     }
 }
